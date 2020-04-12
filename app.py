10c41655cba69d81b2a53321f9f6440c0bf02614
karsh <karsharma24@gmail.com>
2020-04-11 18:11:58 -0600

    Init logs service. For now, an array to do CRUD on.

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask
 from flask import request
 from flask.json import jsonify
+from service.logs_service import get_all_logs, get_a_log, add_log
 app = Flask(__name__)
 
 
-@app.route('/', methods = [ 'POST' ])
-def hello():
+@app.route('/logs')
+def GetLogs():
+    return jsonify(get_all_logs())
+
+@app.route('/logs', methods=['POST'])
+def AddLog():
     data = request.get_json()
-    return jsonify(data)
+    add_log(data)
+    return jsonify({ 'success': True }), 200
+
 
 if __name__ == '__main__':
     app.run()
---
+++ service/logs_service.py
@@ -0,0 +1,7 @@
+
+logs = []
+def get_all_logs():
+    return logs
+
+def add_log(data):
+    logs.append(data)
