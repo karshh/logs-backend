b873fc27751cbff18b2c7c6b0e596f31bf84689d
karsh <karsharma24@gmail.com>
2020-04-11 22:01:08 -0600

    Added a 'from' and 'to' query option

--- services/logs_service.py
+++ services/logs_service.py
@@ -6,10 +6,21 @@ def get_logs(userId, from_date, to_date, typesList):
 
     pipeline = []
     match = []
+    filter = []
     
     pipeline.append({ "$project": { "_id": 0 }})
     if userId: match.append({ 'userId': userId })
+    if from_date: 
+        filter.append({ '$gte' : ['$$action.time', from_date ]})
+        match.append({ 'actions.time': { '$gte': from_date } })
+    if to_date: 
+        filter.append({ '$lte' : ['$$action.time', to_date ]})
+        match.append({ 'actions.time': { '$lte': to_date } })
     if typesList: 
+        filter.append({ '$in' : ['$$action.type', typesList ]})
+        match.append({ 'actions.type': {'$in': typesList} })
+    if match: pipeline.append( {"$match": { "$and": match } } )
+    if filter: 
         pipeline.append({ 
             "$project": 
             {
@@ -19,15 +30,13 @@ def get_logs(userId, from_date, to_date, typesList):
                     {
                         "input": "$actions",
                         "as": "action",
-                        "cond":{"$in": ['$$action.type', typesList ]}
+                        "cond":{"$and": filter }
                     }
                 },
                 "userId": 1,
                 "sessionId": 1
             }
         })
-        match.append({ 'actions.type': {'$in': typesList} })
-    if match: pipeline.append( {"$match": { "$and": match } } )
     cursor = mongo.db.logs.aggregate(pipeline)
     return loads(dumps(cursor))
 
